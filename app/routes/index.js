be5a0085b6b1fc4393f83249ca9b13e0ec9ecd23
DevNoteKeeper <seoyoon.developer@gmail.com>
2026-05-20 19:12:38 +0200

    modify code based on Exercise 4(regular)

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,17 +8,47 @@ var db = pgp(
 
 router.get("/", async function (req, res, next) {
   // res.render("index", { title: "Express" });
+  // try {
+  //   const data = await db.any(
+  //     `SELECT g.student_id, g.grade, g.quarter, g.year, g.course_code, g.course
+  //       FROM srs.grades AS g
+  //       WHERE g.student_id = 1010191
+  //       ORDER BY g.year ASC, g.quarter ASC`,
+  //   );
+  //   res.render("index", {
+  //     title: "Grades of student 1010191",
+  //     grades: data,
+  //   });
+  // } catch (err) {
+  //   console.error(err);
+  //   next(err);
+  // }
+
+  res.render("index", {
+    title: "Student grades",
+    grades: [],
+    student_id: "",
+  });
+});
+
+router.post("/", async function (req, res, next) {
+  const student_id = req.body.student_id;
 
   try {
     const data = await db.any(
-      `SELECT g.student_id, g.grade, g.quarter, g.year, g.course_code, g.course
-        FROM srs.grades AS g
-        WHERE g.student_id = 1010191
-        ORDER BY g.year ASC, g.quarter ASC`,
+      `
+      SELECT g.student_id, g.grade, g.quarter, g.year, g.course_code, g.course
+      FROM srs.grades AS g
+      WHERE g.student_id = 1010191
+      ORDER BY g.year, g.quarter
+      `,
+      [student_id],
     );
+
     res.render("index", {
-      title: "Grades of student 1010191",
+      title: "Student grades",
       grades: data,
+      student_id: student_id,
     });
   } catch (err) {
     console.error(err);
