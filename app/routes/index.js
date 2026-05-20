f4d4ba6eacc94511635de0e0d330256a977ed810
DevNoteKeeper <seoyoon.developer@gmail.com>
2026-05-20 18:31:09 +0200

    Initial commit

---
+++ app/routes/index.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+var pgp = require("pg-promise")();
+
+var db = pgp(
+  "postgres://dab_dda2526-2b_34:qjVKYu5b5X7o97qF@bronto.ewi.utwente.nl:5432/dab_dda2526-2b_34",
+);
+
+router.get("/", async function (req, res, next) {
+  // res.render("index", { title: "Express" });
+
+  try {
+    const data = await db.any(
+      `SELECT g.student_id, g.grade, g.quarter, g.year, g.course_code, g.course
+        FROM srs.grades AS g
+        WHERE g.student_id = 1010191
+        ORDER BY g.year ASC, g.quarter ASC`,
+    );
+    res.render("index", {
+      title: "Grades of student 1010191",
+      grades: data,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+module.exports = router;
